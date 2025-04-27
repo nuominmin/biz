f5033fbef4326ab63951368fdac1a796b7e16815
nuominmin <776191753@qq.com>
2025-04-27 05:21:48 +0000

    Add captcha main

---
+++ captcha/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github/nuominmin/biz/captcha"
+	"strings"
+)
+
+func main() {
+	cap := captcha.NewCaptcha()
+
+	// 生成验证码
+	id, b64s := cap.Generate()
+	fmt.Println("验证码 ID：", id)
+
+	// 打印提示
+	fmt.Println("请在浏览器打开以下链接，查看验证码图片内容：")
+	fmt.Printf("%s\n", b64s)
+
+	// 让用户手动输入验证码
+	var answer string
+	fmt.Print("请输入验证码内容：")
+	fmt.Scanln(&answer)
+
+	// 验证输入是否正确
+	if !cap.Verify(id, strings.TrimSpace(answer)) {
+		fmt.Println("验证码验证失败！")
+	} else {
+		fmt.Println("验证码验证成功 ✅")
+	}
+}
